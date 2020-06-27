257ac96578d216a08cc9a2033874963965e2a149
vmanz73 <firman.syahmaulana@yahoo.com>
2020-06-27 20:10:34 +0700

    update laporan

--- Code/vigenere.py
+++ Code/vigenere.py
@@ -1,10 +1,4 @@
-# Python code to implement 
-# Vigenere Cipher 
 
-# This function generates the 
-# key in a cyclic manner until 
-# it's length isn't equal to 
-# the length of original text 
 def generateKey(string, key): 
 	key = list(key) 
 	if len(string) == len(key): 
@@ -15,9 +9,7 @@ def generateKey(string, key):
 			key.append(key[i % len(key)]) 
 	return("" . join(key)) 
 	
-# This function returns the 
-# encrypted text generated 
-# with the help of the key 
+
 def cipherText(string, key): 
 	cipher_text = [] 
 	for i in range(len(string)): 
@@ -27,9 +19,7 @@ def cipherText(string, key):
 		cipher_text.append(chr(x)) 
 	return("" . join(cipher_text)) 
 	
-# This function decrypts the 
-# encrypted text and returns 
-# the original text 
+
 def originalText(cipher_text, key): 
 	orig_text = [] 
 	for i in range(len(cipher_text)): 
